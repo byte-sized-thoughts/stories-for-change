34d48610ef2fafa115073e861863e56a16f07201
byte-sized-thoughts <steffi.kohl@me.com>
2024-10-08 21:32:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-document.getElementById("signup-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the default form submission
-
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const affiliation = document.getElementById("affiliation").value;
-    const message = document.getElementById("message").value;
-
-    // You can replace the following alert with an AJAX call to your server to handle form submission
-    alert(`Thank you for signing up, ${name}! We will contact you at ${email}.`);
-
-    // Clear the form fields
-    document.getElementById("signup-form").reset();
-});
+gsap.timeline({
+  scrollTrigger:{
+    trigger:'.scrollDist',
+    start:'0 0',
+    end:'100% 100%',
+    scrub:1
+  }
+})
+.fromTo('.sky', {y:0},{y:-200}, 0)
+.fromTo('.cloud1', {y:100},{y:-800}, 0)
+.fromTo('.cloud2', {y:-150},{y:-500}, 0)
+.fromTo('.cloud3', {y:-50},{y:-650}, 0)
+.fromTo('.mountBg', {y:-10},{y:-100}, 0)
+.fromTo('.mountMg', {y:-30},{y:-250}, 0)
+.fromTo('.mountFg', {y:-50},{y:-600}, 0);
