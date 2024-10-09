b0f7c527913aaf5d047374ec5ae84ecd0782b9a6
byte-sized-thoughts <steffi.kohl@me.com>
2024-10-09 14:38:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
   })
   .fromTo('.background', { y: 0 }, { y: -200 }, 0)
   .fromTo('.clouds', { y: 170 }, { y: -400 }, 0)
-  .fromTo('.cloud_main', { y: 170 }, { y: -470 }, 0)
+  .fromTo('.cloud_main', { y: 170 }, { y: -300 }, 0)
   .fromTo('.balloon', { y: 0 }, { y: -200 }, 0)
   .fromTo('.sky', { y: 0 }, { y: -200 }, 0)
   .fromTo('.book', { y: 0 }, { y: -600 }, 0);
