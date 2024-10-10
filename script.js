f3ef6f48cf6bf1ce5d6e5f1991a38e6a9266a69d
byte-sized-thoughts <steffi.kohl@me.com>
2024-10-10 22:36:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,15 @@
-body, html {
-  margin: 0;
-  padding: 0;
-  font-family: 'Overlock', Arial, sans-serif;
+// Toggle Menu Function
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('active');
 }
 
-#navbar {
-  position: fixed;
-  width: 100%;
-  background-color: #181d23;
-}
-
-.menu-button {
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-}
-
-.menu-container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.8);
-  display: none;
-}
-
-.menu-container.active {
-  display: block;
-}
-
-.container {
-  text-align: center;
-  padding: 50px;
-}
-
-#welcome-section {
-  height: 100vh;
-  background-color: #202736;
-  color: #fafafa;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-
-footer {
-  text-align: center;
-  padding: 10px;
-  background-color: #fafafa;
-}
+// Smooth Scrolling for Internal Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
